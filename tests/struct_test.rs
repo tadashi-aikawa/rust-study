27a4cb651c093fda30b1e4c28ee6db78449724c8
tadashi-aikawa <syou.maman@gmail.com>
2022-12-31 19:25:00 +0900

    ロガーの挙動 追加

--- tests/struct_test.rs
+++ tests/struct_test.rs
@@ -27,3 +27,45 @@ mod シンプル {
         assert_eq!("id = 10, name = mike", actual);
     }
 }
+
+#[derive(Debug)]
+struct Logger {
+    logs: Vec<String>,
+}
+impl Logger {
+    fn new() -> Self {
+        Logger { logs: vec![] }
+    }
+
+    fn append(&mut self, log: String) {
+        self.logs.push(log);
+    }
+
+    fn show(&self) {
+        println!("{}", self.logs.join("\n"));
+    }
+}
+
+mod ロガーの挙動 {
+    use super::*;
+
+    #[test]
+    fn ロガーにログを2つ以上追加できる() {
+        let mut logger = Logger::new();
+        logger.append("log1".to_string());
+        logger.append("log2".to_string());
+        logger.show();
+    }
+
+    #[test]
+    fn クロージャーの内外でログを2つ以上追加できる() {
+        let mut logger = Logger::new();
+        logger.append("log1".to_string());
+        vec![String::from("AAA"), String::from("BBB")]
+            .into_iter()
+            .for_each(|x| {
+                logger.append(x);
+            });
+        logger.show();
+    }
+}

69431ea4da4f41d75a6cb15d246baae1c50bd3fa
tadashi-aikawa <syou.maman@gmail.com>
2023-01-16 11:38:38 +0900

    文字列フォーマットのユースケースを追加 (+editorconfigでLF改行を強制)

--- tests/string_test.rs
+++ tests/string_test.rs
@@ -10,3 +10,35 @@ mod 文字列の変換 {
         assert_eq!("cc", &string[4..]);
     }
 }
+
+mod フォーマット {
+    #[test]
+    fn ゼロ埋めや空白埋めできる() {
+        let x = "100";
+
+        // 右寄せ8桁
+        assert_eq!(format!("{x:>8} end"), "     100 end");
+        // 左寄せ8桁
+        assert_eq!(format!("{x:<8} end"), "100      end");
+        // 中央寄せ8桁
+        assert_eq!(format!("{x:^8} end"), "  100    end");
+        // ゼロ埋め右寄せ8桁
+        assert_eq!(format!("{x:0>8} end"), "00000100 end");
+        // *埋め左寄せ8桁
+        assert_eq!(format!("{x:*<8} end"), "100***** end");
+    }
+
+    #[test]
+    fn 浮動小数点の精度_桁数を指定できる() {
+        let x = 12.345;
+
+        assert_eq!(format!("{x:.0}"), "12");
+        // 四捨五入
+        assert_eq!(format!("{x:.1}"), "12.3");
+        // 四捨五入
+        assert_eq!(format!("{x:.2}"), "12.35");
+        assert_eq!(format!("{x:.3}"), "12.345");
+        // 余った分は0で
+        assert_eq!(format!("{x:.4}"), "12.3450");
+    }
+}
